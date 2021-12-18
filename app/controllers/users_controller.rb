b85f81a1c0ba34424906c78dbf2b766fc2f110c3
jibirian999 <jehiapl999@gmail.com>
2021-12-18 11:31:57 +0900

    feat: User画像アップロード機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :image, :password, :password_confirmation)
     end
 end
