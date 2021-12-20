7a0c8df4de00811b109dd30b9946973818ec45f7
jibirian999 <jehiapl999@gmail.com>
2021-12-20 21:28:39 +0900

    feat: User一覧機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update]
+  before_action :logged_in_user, only: %i[index edit update]
   before_action :correct_user,   only: %i[edit update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
