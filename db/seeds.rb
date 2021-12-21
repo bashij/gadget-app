d70c34e2f262073ba5af94f64cb1726afcbaa3be
jibirian999 <jehiapl999@gmail.com>
2021-12-21 21:24:28 +0900

    feat: User,Tweetサンプルデータ作成処理をseeds.rbに追加/fakerの日本語化

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# メインのサンプルユーザーを作成
+User.create!(name: 'bashi',
+             email: 'bashi@gmail.com',
+             password: 'foobar',
+             password_confirmation: 'foobar')
+
+# その他のサンプルユーザーを作成
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n + 1}@gmail.com"
+  password = 'password'
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+# ユーザーの一部を対象にサンプルツイートを作成
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.paragraph_by_chars(number: 100)
+  users.each { |user| user.tweets.create!(content: content) }
+end
