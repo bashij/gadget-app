a2c046d157edd99cf22e2a7f274f1c2556948234
jibirian999 <jehiapl999@gmail.com>
2021-12-23 22:49:10 +0900

    feat: フォロー・フォロー解除および各一覧機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
+  has_many :active_relationships,  class_name: 'Relationship',
+                                   foreign_key: 'follower_id',
+                                   inverse_of: 'follower',
+                                   dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   inverse_of: 'followed',
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
@@ -45,5 +55,20 @@ class User < ApplicationRecord
   # feed（仮）
   def feed
     Tweet.where(user_id: id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
