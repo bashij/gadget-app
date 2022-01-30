4e0695ada3200760b93fb5ef6281b685bf0cbac0
jibirian999 <jehiapl999@gmail.com>
2022-01-30 14:17:30 +0900

    feat: Membershipコントローラを追加_#19

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     resources :comments, only: %i[create destroy]
     resource :review_requests, only: %i[create destroy show]
   end
-  resources :communities
+  resources :communities do
+    resource :memberships, only: %i[create destroy]
+  end
   resources :relationships, only: %i[create destroy]
 end
