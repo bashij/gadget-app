264ea4b5fa57fa73376004d8efba611e975efbbd
jibirian999 <jehiapl999@gmail.com>
2022-02-05 14:38:53 +0900

    style: デフォルトの言語を日本語に変更_#33

--- config/application.rb
+++ config/application.rb
@@ -21,18 +21,18 @@ Bundler.require(*Rails.groups)
 
 module GadgetApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    # デフォルト設定で初期化
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # タイムゾーンを日本とする
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
-    # Don't generate system test files.
+    # デフォルトの言語を日本語とする
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+    # テストを自動生成しない
     config.generators.system_tests = nil
   end
 end
