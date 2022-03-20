8879c7ca7125aaccaba2ba906b821e866dd08a82
jibirian999 <jehiapl999@gmail.com>
2022-03-20 17:46:58 +0900

    style: UI整備・Ajax化（memberships）_#33

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,13 +1,17 @@
 class MembershipsController < ApplicationController
+  before_action :logged_in_user, only: %i[create destroy]
+
   def create
     membership = current_user.memberships.build(community_id: params[:community_id])
     membership.save
-    redirect_to request.referer || root_url
+    @community = Community.find(params[:community_id])
+    @users = @community.joined_members.page(params[:users_page]).per(10)
   end
 
   def destroy
     membership = Membership.find_by(community_id: params[:community_id], user_id: current_user.id)
     membership.destroy
-    redirect_to request.referer || root_url
+    @community = Community.find(params[:community_id])
+    @users = @community.joined_members.page(params[:users_page]).per(10)
   end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,5 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :community
+  default_scope -> { order(created_at: :desc) }
 end
