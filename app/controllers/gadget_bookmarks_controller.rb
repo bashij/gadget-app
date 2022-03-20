ac497cd3545388db679dd1c9fee4f7b455c35040
jibirian999 <jehiapl999@gmail.com>
2022-03-20 17:40:29 +0900

    style: UI整備・Ajax化（gadget_bookmarks）_#33

--- app/controllers/gadget_bookmarks_controller.rb
+++ app/controllers/gadget_bookmarks_controller.rb
@@ -1,13 +1,15 @@
 class GadgetBookmarksController < ApplicationController
+  before_action :logged_in_user, only: %i[create destroy]
+
   def create
     bookmark = current_user.gadget_bookmarks.build(gadget_id: params[:gadget_id])
     bookmark.save
-    redirect_to request.referer || root_url
+    @gadget = Gadget.find(params[:gadget_id])
   end
 
   def destroy
     bookmark = GadgetBookmark.find_by(gadget_id: params[:gadget_id], user_id: current_user.id)
     bookmark.destroy
-    redirect_to request.referer || root_url
+    @gadget = Gadget.find(params[:gadget_id])
   end
 end
