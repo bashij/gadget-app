33ca7dd8347c289f88c9ae40b2cc9eceb7b29295
jibirian999 <jehiapl999@gmail.com>
2022-03-20 17:46:04 +0900

    style: UI整備・Ajax化（communities）_#33

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,12 +2,15 @@ class Community < ApplicationRecord
   belongs_to :user
   has_many :memberships, dependent: :destroy
   has_many :joined_members, through: :memberships, source: :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
   mount_uploader :image, CommunityImageUploader
 
   # ユーザーが既に参加しているか？
   def joined_by?(user)
+    return false if user.nil?
+
     memberships.pluck(:user_id).include?(user.id)
   end
 end
