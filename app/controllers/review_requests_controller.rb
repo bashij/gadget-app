65946a41d0e2f427b963dcf1344b3e44a92a007e
jibirian999 <jehiapl999@gmail.com>
2022-03-20 17:42:56 +0900

    style: UI整備・Ajax化（review_requests）_#33

--- app/controllers/review_requests_controller.rb
+++ app/controllers/review_requests_controller.rb
@@ -1,17 +1,24 @@
 class ReviewRequestsController < ApplicationController
+  before_action :logged_in_user, only: %i[create destroy]
+
   def show
-    @users = Gadget.find(params[:gadget_id]).requesting_users
+    @users = Gadget.find(params[:gadget_id]).requesting_users.page(params[:users_page]).per(10)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
     review_request = current_user.review_requests.build(gadget_id: params[:gadget_id])
     review_request.save
-    redirect_to request.referer || root_url
+    @gadget = Gadget.find(params[:gadget_id])
   end
 
   def destroy
     review_request = ReviewRequest.find_by(gadget_id: params[:gadget_id], user_id: current_user.id)
     review_request.destroy
-    redirect_to request.referer || root_url
+    @gadget = Gadget.find(params[:gadget_id])
   end
 end
