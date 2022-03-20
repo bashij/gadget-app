693dbd22d17ddfe126d4d4f78559aaefd2b249ab
jibirian999 <jehiapl999@gmail.com>
2022-03-20 17:08:40 +0900

    style: UI整備・Ajax化（sessions）_#33

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      redirect_back_or('/')
     else
       flash.now[:danger] = '無効なメールアドレスまたはパスワードです'
       render 'new'
