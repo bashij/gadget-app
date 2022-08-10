3a1629dfd6a20e8f80e0484b5696e1ea6b85c8bc
jibirian999 <jehiapl999@gmail.com>
2022-08-10 20:11:51 +0900

    style: idは'_'、classは'-'に統一する_#45

--- spec/factories/communities.rb
+++ spec/factories/communities.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :community do
     user
-    name  { 'Appleファン' }
+    name  { Faker::Lorem.paragraph_by_chars(number: 10) }
     image { Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/default.jpeg')) }
   end
 end
