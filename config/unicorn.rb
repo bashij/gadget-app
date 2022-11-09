e9fa4c71158208effef6c34a8e2122d0da380775
jibirian999 <jehiapl999@gmail.com>
2022-11-09 21:18:28 +0900

    chore: 本番環境 Nginx用の設定を追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 timeout 60
