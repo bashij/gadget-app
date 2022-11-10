bc47d8edc57afbb74f85b300cdacc56f0ddaa650
jibirian999 <jehiapl999@gmail.com>
2022-11-10 20:34:48 +0900

    chore: Capistrano用のmaster.key設定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :deploy_to, '/var/www/gadget-app'
 set :repo_url,  'git@github.com:jibirian999/gadget-app.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '3.0.2'
