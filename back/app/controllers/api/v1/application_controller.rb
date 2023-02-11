57a8c0e7cc366901f9bb0b634b67f39643ecbf5d
jibirian999 <jehiapl999@gmail.com>
2023-02-11 15:51:58 +0900

    feat: application_controllerをAPI用に変更_#73

---
+++ back/app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class ApplicationController < ActionController::API
+      include SessionsHelper
+
+      private
+
+        # ログイン済みユーザーかどうか確認
+        def logged_in_user
+          return if logged_in?
+
+          store_location
+          flash[:danger] = t 'sessions.flash.danger'
+          respond_to do |format|
+            format.html { redirect_to login_path }
+            format.js { render js: "window.location = #{login_path.to_json}" }
+          end
+        end
+    end
+  end
+end
