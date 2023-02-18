23b94344f965c9396c7230b7ef22d5961d903837
jibirian999 <jehiapl999@gmail.com>
2023-02-18 14:41:44 +0900

    style: FontAwesome導入、ヘッダーのスタイル調整、レスポンシブ化_#73

--- front/app/components/layout.js
+++ front/app/components/layout.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React, {useState} from "react"
 
 export const siteTitle = 'GadgetLink';
 
@@ -18,7 +21,7 @@ export default function Layout({ children, home }) {
           </div>
           {/* メニュー 通常時 */}
           <div className="menu d-none d-lg-block col-lg-9 text-end">
-            <Link href="" className='d-inline-block nav-link' id="home_large">HOME</Link>
+            <Link href="/" className='d-inline-block nav-link' id="home_large">HOME</Link>
             <Link href="" className='d-inline-block nav-link' id="help_large">HELP</Link>
             <Link href="users" className='d-inline-block nav-link' id="users_large">USERS</Link>
             {/* ログイン時のみ表示 */}
@@ -29,24 +32,7 @@ export default function Layout({ children, home }) {
             <Link href="" className='d-inline-block nav-link' id="login_large">LOGIN</Link>
           </div>
           {/* メニュー 縮小時 */}
-          <div className="menu-btn fs-2 col-9 d-lg-none d-inline-block pe-3 text-end">
-            <a id="header_menu" className="" data-bs-toggle="collapse" href="#nav_header" aria-expanded="false" aria-controls="nav_header" data-bs-placement="top" title="返信フォームを開く">
-              <i className="fa-solid fa-bars">a</i>
-            </a>
-          </div>
-          <div className="collapse col-12 d-lg-none text-end" id="nav_header">
-            <div className="menu d-none d-lg-block col-lg-9 text-end">
-              <Link href="" className='d-inline-block nav-link' id="home_small">HOME</Link>
-              <Link href="" className='d-inline-block nav-link' id="help_small">HELP</Link>
-              <Link href="" className='d-inline-block nav-link' id="users_small">USERS</Link>
-              {/* ログイン時のみ表示 */}
-              <Link href="" className='d-inline-block nav-link' id="mypage_small">MYPAGE</Link>
-              <Link href="" className='d-inline-block nav-link' id="setting_small">SETTING</Link>
-              <Link href="" className='d-inline-block nav-link' id="logout_small">LOGOUT</Link>
-              {/* 非ログイン時のみ表示 */}
-              <Link href="" className='d-inline-block nav-link' id="login_small">LOGIN</Link>
-            </div>
-          </div>
+          <SmallMenu />
       </header>
       <div className="container"> 
         {/* success,errorに応じたメッセージをここで受け取って表示する。各POSTの返り値が使えるか。 */}
@@ -59,4 +45,30 @@ export default function Layout({ children, home }) {
       )}
     </>
   );
+}
+
+export function SmallMenu() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    setOpenMenu(!openMenu)
+  }
+
+  return (
+    <>
+      <div className='fs-2 col-9 d-lg-none d-inline-block pe-3 text-end'>
+        <FontAwesomeIcon icon={faBars} className='menu-btn' onClick={() => menuFunction()}/>
+      </div>
+      <div id="nav_header" className={`col-12 d-lg-none text-end drawerMenu ${openMenu ? 'open' : 'hidden'}`}>
+        <Link href="/" className='d-block nav-link' id="home_small">HOME</Link>
+        <Link href="" className='d-block nav-link' id="help_small">HELP</Link>
+        <Link href="users" className='d-block nav-link' id="users_small">USERS</Link>
+        {/* ログイン時のみ表示 */}
+        <Link href="" className='d-block nav-link' id="mypage_small">MYPAGE</Link>
+        <Link href="" className='d-block nav-link' id="setting_small">SETTING</Link>
+        <Link href="" className='d-block nav-link' id="logout_small">LOGOUT</Link>
+        {/* 非ログイン時のみ表示 */}
+        <Link href="" className='d-block nav-link' id="login_small">LOGIN</Link>
+      </div>
+    </>
+  )
 }
