24d7d94f41ff34f08169e463fc4ac3906d9c5a91
jibirian999 <jehiapl999@gmail.com>
2023-08-28 19:30:11 +0900

    test: トップページのテストを追加_#75

---
+++ front/app/__tests__/index.test.js
@@ -0,0 +1,30 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    query: { message: ["initialMessage"], status: "initialStatus" },
+  }),
+}));
+
+describe("Home", () => {
+  it("ヘッダーが表示されている", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", { name: "GadgetLink" });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("各ボタンが適切なタグで表示されている", () => {
+    render(<Home />);
+
+    const link1 = screen.getByRole("link", { name: "ログイン" });
+    const link2 = screen.getByRole("link", { name: "新規登録" });
+
+    expect(link1).toBeInTheDocument();
+    expect(link2).toBeInTheDocument();
+  });
+});
