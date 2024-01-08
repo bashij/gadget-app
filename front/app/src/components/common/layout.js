51d0199cc9c53021b0cb9ec19e8dc175f08ce0e9
jibirian999 <jehiapl999@gmail.com>
2024-01-08 09:32:32 +0900

    feat: ロゴ押下時の遷移先をhomeに変更_#116

--- front/app/src/components/common/layout.js
+++ front/app/src/components/common/layout.js
@@ -62,7 +62,7 @@ export default function Layout(props) {
       <div className='container'>
         <div className='row'>
           <div className={`col-sm-3 side-menu ${openMenu ? 'open' : ''}`}>
-            <Link href='/gadgets' className='logo nav-link'>
+            <Link href='/' className='logo nav-link'>
               GadgetLink
             </Link>
             <Link
