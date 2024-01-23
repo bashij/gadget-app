2015d58cac9d4f0da0955a0de40257bbf8d6305f
jibirian999 <jehiapl999@gmail.com>
2024-01-23 09:18:50 +0900

    feat: サンプルデータ作成用seedsを最新の本番仕様かつリアルなデータに変更_#121

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,102 +1,167 @@
-# メインのサンプルユーザーを作成
-User.create!(name: 'bashi',
-             email: 'bashi@example.com',
-             job: 'IT系',
-             password: 'foobar',
-             password_confirmation: 'foobar')
+require "csv"
 
-# その他のサンプルユーザーを作成
-99.times do |n|
-  name  = Faker::Name.name
-  email = "sample#{n + 1}@example.com"
-  password = 'password'
-  job = %w[IT系 非IT系 YouTuber/ブロガー 学生 その他].sample
-  User.create!(name: name,
-               email: email,
-               job: job,
-               password: password,
-               password_confirmation: password)
+# メインユーザーを作成
+CSV.foreach('db/seeds/csv/users.csv', headers: true) do |row|
+  User.create!(
+    name: row['name'],
+    email: row['email'],
+    job: row['job'],
+    introduction: row['introduction'] || "",
+    password: row['password'],
+    password_confirmation: row['password_confirmation']
+  )
 end
 
-# ユーザーの一部を対象に、サンプルツイート/ガジェット/リプライ/いいね/ブックマーク/コミュニティを作成
-users = User.order(:created_at).take(6)
+# メインユーザーを取得
+users = User.where("email LIKE ?", "%example%").order(:created_at).take(8)
 
-# ツイート 新規作成
+# フォロー関係を作成
+users.each do |user|
+  CSV.foreach("db/seeds/csv/relationships.csv", headers: true) do |row|
+    if row['user_email'] == user.email
+      id = User.find_by(email: row['followed_user_email'])
+      user.follow(id)
+    end
+  end
+end
+
+# ガジェットを作成
+users.each do |user|
+  CSV.foreach("db/seeds/csv/gadgets.csv", headers: true) do |row|
+    if row['user_email'] == user.email
+      user.gadgets.create!(
+        name: row['name'],
+        category: row['category'],
+        model_number: row['model_number'] || "",
+        manufacturer: row['manufacturer'] || "",
+        price: row['price'] || 0,
+        other_info: row['other_info'] || "",
+        review: row['review'] || ""
+      )
+    end
+  end
+end
+
+# ランダムに更新（タイムラインでユーザーごとに固まってしまうため）
 50.times do
-  content = Faker::Lorem.paragraph_by_chars(number: 100)
-  users.each do |user|
-    # ツイート
-    user.tweets.create!(content: content)
+  target_gadget = Gadget.all
+  target_gadget.sample.touch
+end
+
+# コミュニティを作成
+users.each do |user|
+  CSV.foreach("db/seeds/csv/communities.csv", headers: true) do |row|
+    if row['user_email'] == user.email
+      user.communities.create!(
+        name: row['name'],
+      )
+    end
   end
 end
 
-# ガジェット・コミュニティ 新規作成
-count = 1
-5.times do
-  users.each do |user|
-    # ガジェット
-    name = "MacBook Pro #{user.id}"
-    category = %w[PC本体 モニター キーボード マウス オーディオ デスク チェア その他].sample
-    model_number = "modelNo #{user.id}"
-    manufacturer = "Apple #{user.id}"
-    price = 200_000
-    other_info = "メモリ:16GB #{user.id}"
-    review = "#{user.name} #{Faker::Lorem.paragraph_by_chars(number: 2000)}"
-    user.gadgets.create!(name: name,
-                         category: category,
-                         model_number: model_number,
-                         manufacturer: manufacturer,
-                         price: price,
-                         other_info: other_info,
-                         review: review)
-    # コミュニティ    
-    name = "#{user.name} コミュニティ #{count}"
-    user.communities.create!(name: name)    
-  end
-  count += 1
-end
-
-# ユーザーの一部が作成したツイートの最新IDを取得
-tweet_reply_target_id = users.last.tweets.ids[0]
-
-# ツイート：リプライ/いいね/ブックマーク
+# ランダムに更新（タイムラインでユーザーごとに固まってしまうため）
 20.times do
-  tweet_content = "リプライ: #{Faker::Lorem.paragraph_by_chars(number: 100)}"
-  users.each do |user|
-    # ツイート
-    user.tweets.create!(content: tweet_content, parent_id: tweet_reply_target_id)
-    user.tweet_likes.create!(tweet_id: tweet_reply_target_id)
-    user.tweet_bookmarks.create!(tweet_id: tweet_reply_target_id)
-  end
-  tweet_reply_target_id -= 1
+  target_gadget = Community.all
+  target_gadget.sample.touch
 end
 
-# ユーザーの一部が作成したガジェット/コミュニティの最新IDを取得
-gadget_reply_target_id = users.last.gadgets.ids[0]
-community_join_target_id = users.last.communities.ids[0]
-
-# ガジェット：コメント/リプライ/いいね/ブックマーク/リクエスト コミュニティ：新規加入
-5.times do
-  # community_sample = Community.all.ids
-  # gadget_sample = Gadget.all.ids
-  comment_content = "コメント: #{Faker::Lorem.paragraph_by_chars(number: 100)}"
-  users.each do |user|
-    # ガジェット
-    user.comments.create!(gadget_id: gadget_reply_target_id, content: comment_content)
-    user.gadget_likes.create!(gadget_id: gadget_reply_target_id)
-    user.gadget_bookmarks.create!(gadget_id: gadget_reply_target_id)
-    user.review_requests.create!(gadget_id: gadget_reply_target_id)
-    # コミュニティ
-    user.memberships.create!(community_id: community_join_target_id)
+# コミュニティへ参加
+users.each do |user|
+  CSV.foreach("db/seeds/csv/memberships.csv", headers: true) do |row|
+    if row['user_email'] == user.email
+      community_id = Community.find_by(name: row['name']).id
+      user.memberships.create!(community_id: community_id)
+    end
   end
-  gadget_reply_target_id -= 1
-  community_join_target_id -= 1
 end
 
-# 以下のリレーションシップを作成する
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+# ツイートとリプライを作成
+# CSVデータ読み込み
+csv_data_hashes = CSV.read("db/seeds/csv/tweets.csv", headers: true).map(&:to_h)
+
+# 親ツイートの処理
+csv_data_hashes.each do |row|
+  next if row['parent_id']
+
+  user = User.find_by(email: row['user_email'])
+  tweet = user.tweets.create!(content: row['content'])
+
+  # リプライツイート用の対照表作成
+  real_parent_id = tweet.id
+  csv_data_hashes.each { |r| r["real_parent_id"] = real_parent_id if r["parent_id"] == row['id'] }
+end
+
+# リプライツイートの処理
+result_hash = csv_data_hashes.each_with_object({}) do |hash, new_hash|
+  id = hash["id"].to_i
+  new_hash[id] = hash["real_parent_id"] if hash['parent_id']
+end
+
+csv_data_hashes.each do |row|
+  next unless row['parent_id']
+
+  user = User.find_by(email: row['user_email'])
+  parent_id = result_hash[row['id'].to_i]
+
+  # 子ツイート作成
+  user.tweets.create!(content: row['content'], parent_id: parent_id)
+end
+
+# コメントとリプライを作成
+# CSVデータ読み込み
+csv_data_hashes = CSV.read("db/seeds/csv/comments.csv", headers: true).map(&:to_h)
+
+# 親コメントの処理
+csv_data_hashes.each do |row|
+  next if row['parent_id']
+  next unless row['user_email']
+  
+  user = User.find_by(email: row['user_email'])
+  gadget = Gadget.joins(:user).find_by(name: row['gadget_name'], user: { email: row['gadget_user_email'] })
+  comment = user.comments.create!(gadget_id: gadget.id, content: row['content'])
+
+  # リプライコメント用の対照表作成
+  real_parent_id = comment.id
+  csv_data_hashes.each { |r| r["real_parent_id"] = real_parent_id if r["parent_id"] == row['id'] }
+end
+
+# リプライコメントの処理
+result_hash = csv_data_hashes.each_with_object({}) do |hash, new_hash|
+  id = hash["id"].to_i
+  new_hash[id] = hash["real_parent_id"] if hash['parent_id']
+end
+
+csv_data_hashes.each do |row|
+  next unless row['parent_id']
+  next unless row['user_email']
+
+  user = User.find_by(email: row['user_email'])
+  parent_id = result_hash[row['id'].to_i]
+  gadget_id = Comment.find(parent_id).gadget_id
+
+  # 子コメント作成
+  user.comments.create!(gadget_id: gadget_id, content: row['content'], parent_id: parent_id)
+end
+
+# ツイートへのアクションを追加
+users.each do |user|
+  CSV.foreach("db/seeds/csv/tweets_actions.csv", headers: true) do |row|
+    if row['user_email'] == user.email
+      tweet = Tweet.find_by(content: row['content'])
+      user.tweet_likes.create!(tweet_id: tweet.id) if row['like']
+      user.tweet_bookmarks.create!(tweet_id: tweet.id) if row['bookmark']
+    end
+  end
+end
+
+# ガジェットへのアクションを追加
+users.each do |user|
+  CSV.foreach("db/seeds/csv/gadgets_actions.csv", headers: true) do |row|
+    if row['user_email'] == user.email
+      gadget = Gadget.joins(:user).find_by(name: row['gadget_name'], user: { email: row['gadget_user_email'] })
+      user.gadget_likes.create!(gadget_id: gadget.id) if row['like']
+      user.gadget_bookmarks.create!(gadget_id: gadget.id) if row['bookmark']
+      user.review_requests.create!(gadget_id: gadget.id) if row['request']
+    end
+  end
+end
